ColumbinaHyposeleniaYS/LiquidBounce

commit 081c3078d2c40b513ebe824c24d55ee711c08b4e

chore(deps): fastutil4k 0.2.6 (#7787)
Added enhanced forEachIsInstance

src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleHud.kt
@@ -18,6 +18,7 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
+import net.ccbluex.fastutil.forEachIsInstance
 import net.ccbluex.liquidbounce.config.ConfigSystem
 import net.ccbluex.liquidbounce.config.types.group.ToggleableValueGroup
 import net.ccbluex.liquidbounce.config.types.group.ValueGroup
@@ -94,7 +95,7 @@ object ModuleHud : ClientModule("HUD", ModuleCategories.RENDER, state = true, hi
      * Updates [themes] content
      */
     fun updateThemes() {
-        themes.inner.filterIsInstance<ValueGroup>().forEach {
+        themes.inner.forEachIsInstance<ValueGroup> {
             themes.drop(it)
         }
         for (theme in ThemeManager.themes) {

gradle/libs.versions.toml
@@ -27,7 +27,7 @@ httpServer = "2.5.2"
 discordIpc = "4.0.1"
 semver4j = "3.1.0"
 ahocorasick = "0.6.3"
-fastutil4k = "0.2.5"
+fastutil4k = "0.2.6"
 
 # Recommended mods
 modmenu = "17.0.0-beta.1"

src/main/kotlin/net/ccbluex/liquidbounce/config/types/group/ValueGroup.kt
@@ -24,6 +24,7 @@ import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
 import com.mojang.blaze3d.platform.InputConstants
 import net.ccbluex.fastutil.enumSetOf
+import net.ccbluex.fastutil.forEachIsInstance
 import net.ccbluex.fastutil.toEnumSet
 import net.ccbluex.liquidbounce.config.ConfigSystem
 import net.ccbluex.liquidbounce.config.types.BindValue
@@ -108,7 +109,7 @@ open class ValueGroup(
         get() = "${ConfigSystem.KEY_PREFIX}.${name.toLowerCamelCase()}"
 
     open fun walkInit() {
-        inner.filterIsInstance<ValueGroup>().forEach { valueGroup ->
+        inner.forEachIsInstance<ValueGroup> { valueGroup ->
             valueGroup.walkInit()
         }
     }

src/main/kotlin/net/ccbluex/liquidbounce/features/blink/BlinkManager.kt
@@ -19,8 +19,9 @@
 package net.ccbluex.liquidbounce.features.blink
 
 import com.google.common.collect.Queues
+import net.ccbluex.fastutil.filterIsInstance
+import net.ccbluex.fastutil.forEachIsInstance
 import net.ccbluex.fastutil.mapToArray
-import net.ccbluex.liquidbounce.config.types.group.ModeValueGroup
 import net.ccbluex.liquidbounce.config.types.group.ValueGroup
 import net.ccbluex.liquidbounce.event.EventListener
 import net.ccbluex.liquidbounce.event.EventManager
@@ -33,10 +34,10 @@ import net.ccbluex.liquidbounce.event.events.TransferOrigin
 import net.ccbluex.liquidbounce.event.events.WorldChangeEvent
 import net.ccbluex.liquidbounce.event.events.WorldRenderEvent
 import net.ccbluex.liquidbounce.event.handler
-import net.ccbluex.liquidbounce.features.blink.esp.AbstractBlinkEspBox
-import net.ccbluex.liquidbounce.features.blink.esp.AbstractBlinkEspModel
-import net.ccbluex.liquidbounce.features.blink.esp.AbstractBlinkEspWireframe
-import net.ccbluex.liquidbounce.features.blink.esp.AbstractBlinkNone
+import net.ccbluex.liquidbounce.features.blink.esp.BlinkEspBox
+import net.ccbluex.liquidbounce.features.blink.esp.BlinkEspModel
+import net.ccbluex.liquidbounce.features.blink.esp.BlinkEspWireframe
+import net.ccbluex.liquidbounce.features.blink.esp.BlinkEspNone
 import net.ccbluex.liquidbounce.features.blink.esp.BlinkEspData
 import net.ccbluex.liquidbounce.render.drawLineStrip
 import net.ccbluex.liquidbounce.render.engine.type.Color4b
@@ -77,24 +78,20 @@ object BlinkManager : EventListener, ValueGroup("BlinkManager") {
     val positions
         get() = packetQueue
             .map { snapshot -> snapshot.packet }
-            .filterIsInstance<ServerboundMovePlayerPacket>()
-            .filter { playerMoveC2SPacket -> playerMoveC2SPacket.hasPos }
+            .filterIsInstance<ServerboundMovePlayerPacket> { playerMoveC2SPacket -> playerMoveC2SPacket.hasPos }
             .map { playerMoveC2SPacket -> Vec3(playerMoveC2SPacket.x, playerMoveC2SPacket.y, playerMoveC2SPacket.z) }
 
     val isLagging
         get() = packetQueue.isNotEmpty()
 
-    private val espMode = modes(
-        this,
-        "Esp",
-        EspWireframe,
+    private val espMode = modes(this, "Esp", 2) {
         arrayOf(
-            EspBox,
-            EspModel,
-            EspWireframe,
-            EspNone
+            BlinkEspBox(it, ::getEspData),
+            BlinkEspModel(it, ::getEspData),
+            BlinkEspWireframe(it, ::getEspData),
+            BlinkEspNone(it),
         )
-    ).apply {
+    }.apply {
         doNotIncludeAlways()
     }
 
@@ -193,7 +190,7 @@ object BlinkManager : EventListener, ValueGroup("BlinkManager") {
         }
     }
 
-    fun getEspData(): BlinkEspData? {
+    private fun getEspData(): BlinkEspData? {
         val pos = positions.firstOrNull() ?: return null
         val rotation = RotationManager.actualServerRotation
 
@@ -203,26 +200,6 @@ object BlinkManager : EventListener, ValueGroup("BlinkManager") {
         }
 
         return BlinkEspData(player, pos, rotation)
-    }
-
-    private object EspBox : AbstractBlinkEspBox(::getEspData) {
-        override val parent: ModeValueGroup<*>
-            get() = espMode
-    }
-
-    private object EspWireframe : AbstractBlinkEspWireframe(::getEspData) {
-        override val parent: ModeValueGroup<*>
-            get() = espMode
-    }
-
-    private object EspModel : AbstractBlinkEspModel(::getEspData) {
-        override val parent: ModeValueGroup<*>
-            get() = espMode
-    }
-
-    private object EspNone : AbstractBlinkNone() {
-        override val parent: ModeValueGroup<*>
-            get() = espMode
     }
 
     @Suppress("unused")
@@ -297,9 +274,7 @@ object BlinkManager : EventListener, ValueGroup("BlinkManager") {
     }
 
     inline fun <reified T> rewrite(action: (T) -> Unit) {
-        packetQueue
-            .filterIsInstance<T>()
-            .forEach(action)
+        packetQueue.forEachIsInstance<T>(action)
     }
 
     private fun flushSnapshot(snapshot: PacketSnapshot) {
@@ -315,11 +290,12 @@ object BlinkManager : EventListener, ValueGroup("BlinkManager") {
     enum class Action(val priority: Int) {
         FLUSH(0),
         PASS(1),
-        QUEUE(2)
+        QUEUE(2),
     }
 
 }
 
+@JvmRecord
 data class PacketSnapshot(
     val packet: Packet<*>,
     val origin: TransferOrigin,

src/main/kotlin/net/ccbluex/liquidbounce/features/blink/esp/BlinkEspMode.kt
@@ -20,6 +20,7 @@
 package net.ccbluex.liquidbounce.features.blink.esp
 
 import net.ccbluex.liquidbounce.config.types.group.Mode
+import net.ccbluex.liquidbounce.config.types.group.ModeValueGroup
 import net.ccbluex.liquidbounce.event.events.WorldRenderEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.render.drawBox
@@ -34,19 +35,23 @@ import net.minecraft.world.entity.Entity
 import net.minecraft.world.phys.AABB
 import net.minecraft.world.phys.Vec3
 
+@JvmRecord
 data class BlinkEspData(val entity: Entity, val pos: Vec3, val rotation: Rotation)
 
-abstract class BlinkEspMode(
+sealed class BlinkEspMode(
     name: String,
-    val getEspData: () -> BlinkEspData?
+    protected val getEspData: () -> BlinkEspData?
 ) : Mode(name)
 
-abstract class AbstractBlinkEspBox(getEspData: () -> BlinkEspData?) : BlinkEspMode("Box", getEspData) {
+class BlinkEspBox(
+    override val parent: ModeValueGroup<*>,
+    getEspData: () -> BlinkEspData?,
+) : BlinkEspMode("Box", getEspData) {
     private val color by color("Color", Color4b(36, 32, 147, 87))
 
     @Suppress("unused")
     private val renderHandler = handler<WorldRenderEvent> { event ->
-        val (entity, pos, rotation) = getEspData() ?: return@handler
+        val (entity, pos, rotation) = this.getEspData() ?: return@handler
 
         val dimensions = entity.getDimensions(entity.pose)
         val d = dimensions.width.toDouble() / 2.0
@@ -61,12 +66,15 @@ abstract class AbstractBlinkEspBox(getEspData: () -> BlinkEspData?) : BlinkEspMo
     }
 }
 
-abstract class AbstractBlinkEspModel(getEspData: () -> BlinkEspData?) : BlinkEspMode("Model", getEspData) {
+class BlinkEspModel(
+    override val parent: ModeValueGroup<*>,
+    getEspData: () -> BlinkEspData?,
+) : BlinkEspMode("Model", getEspData) {
     private val lightAmount by float("LightAmount", 0.3f, 0.01f..1f)
 
     @Suppress("unused")
     private val renderHandler = handler<WorldRenderEvent> { event ->
-        val (entity, pos, rotation) = getEspData() ?: return@handler
+        val (entity, pos, rotation) = this.getEspData() ?: return@handler
 
         val entityRenderer = mc.entityRenderDispatcher.getRenderer(entity)
 
@@ -97,17 +105,20 @@ abstract class AbstractBlinkEspModel(getEspData: () -> BlinkEspData?) : BlinkEsp
     }
 }
 
-abstract class AbstractBlinkEspWireframe(getEspData: () -> BlinkEspData?) : BlinkEspMode("Wireframe", getEspData) {
+class BlinkEspWireframe(
+    override val parent: ModeValueGroup<*>,
+    getEspData: () -> BlinkEspData?,
+) : BlinkEspMode("Wireframe", getEspData) {
     private val color by color("Color", Color4b(36, 32, 147, 87))
     private val outlineColor by color("OutlineColor", Color4b(36, 32, 147, 255))
 
     @Suppress("unused")
     private val renderHandler = handler<WorldRenderEvent> {
-        val (entity, pos, rotation) = getEspData() ?: return@handler
+        val (entity, pos, rotation) = this.getEspData() ?: return@handler
 
         val wireframePlayer = WireframePlayer(pos, rotation.yaw, rotation.pitch)
         wireframePlayer.render(it, color, outlineColor)
     }
 }
 
-abstract class AbstractBlinkNone : BlinkEspMode("None", { null })
+class BlinkEspNone(override val parent: ModeValueGroup<*>) : BlinkEspMode("None", { null })

src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/backtrack/ModuleBacktrack.kt
@@ -18,7 +18,6 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.combat.backtrack
 
-import net.ccbluex.liquidbounce.config.types.group.ModeValueGroup
 import net.ccbluex.liquidbounce.config.types.group.ToggleableValueGroup
 import net.ccbluex.liquidbounce.config.types.list.Tagged
 import net.ccbluex.liquidbounce.event.events.AttackEntityEvent
@@ -29,10 +28,10 @@ import net.ccbluex.liquidbounce.event.events.WorldChangeEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.tickHandler
 import net.ccbluex.liquidbounce.features.blink.BlinkManager
-import net.ccbluex.liquidbounce.features.blink.esp.AbstractBlinkEspBox
-import net.ccbluex.liquidbounce.features.blink.esp.AbstractBlinkEspModel
-import net.ccbluex.liquidbounce.features.blink.esp.AbstractBlinkEspWireframe
-import net.ccbluex.liquidbounce.features.blink.esp.AbstractBlinkNone
+import net.ccbluex.liquidbounce.features.blink.esp.BlinkEspBox
+import net.ccbluex.liquidbounce.features.blink.esp.BlinkEspModel
+import net.ccbluex.liquidbounce.features.blink.esp.BlinkEspWireframe
+import net.ccbluex.liquidbounce.features.blink.esp.BlinkEspNone
 import net.ccbluex.liquidbounce.features.blink.esp.BlinkEspData
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.features.module.ModuleCategories
@@ -82,14 +81,14 @@ object ModuleBacktrack : ClientModule("Backtrack", ModuleCategories.COMBAT) {
         RANGE("Range")
     }
 
-    private val espMode = choices(
-        "Esp", BacktrackWireframe, arrayOf(
-            BacktrackBox,
-            BacktrackModel,
-            BacktrackWireframe,
-            EspNone
+    private val espMode = choices("Esp", 2) {
+        arrayOf(
+            BlinkEspBox(it, ::getEspData),
+            BlinkEspModel(it, ::getEspData),
+            BlinkEspWireframe(it, ::getEspData),
+            BlinkEspNone(it),
         )
-    ).apply {
+    }.apply {
         doNotIncludeAlways()
     }
 
@@ -182,32 +181,12 @@ object ModuleBacktrack : ClientModule("Backtrack", ModuleCategories.COMBAT) {
         event.action = BlinkManager.Action.QUEUE
     }
 
-    fun getEspData(): BlinkEspData? {
+    private fun getEspData(): BlinkEspData? {
         val entity = target ?: return null
         val pos = position.base
         val rotation = entity.rotation
 
         return BlinkEspData(entity, pos, rotation)
-    }
-
-    private object BacktrackBox : AbstractBlinkEspBox(::getEspData) {
-        override val parent: ModeValueGroup<*>
-            get() = espMode
-    }
-
-    private object BacktrackWireframe : AbstractBlinkEspWireframe(::getEspData) {
-        override val parent: ModeValueGroup<*>
-            get() = espMode
-    }
-
-    private object BacktrackModel : AbstractBlinkEspModel(::getEspData) {
-        override val parent: ModeValueGroup<*>
-            get() = espMode
-    }
-
-    private object EspNone : AbstractBlinkNone() {
-        override val parent: ModeValueGroup<*>
-            get() = espMode
     }
 
     @Suppress("unused")

src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/crystalaura/CrystalAuraDamageOptions.kt
@@ -82,10 +82,10 @@ object CrystalAuraDamageOptions : ValueGroup("Damage") {
             val friends =
                 world
                     .getEntitiesBoxInRange(pos, 6.0) { FriendManager.isFriend(it) && it.boundingBox.maxY > pos.y }
-                    .filterIsInstance<LivingEntity>()
 
             if (friends.any {
-                it.getDamage(pos, requestingSubmodule, CheckedEntity.OTHER).isGreaterThan(maxFriendDamage)
+                it is LivingEntity &&
+                    it.getDamage(pos, requestingSubmodule, CheckedEntity.OTHER).isGreaterThan(maxFriendDamage)
             }) {
                 tooMuchDamageForFriend = true
             }

src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/autobuff/features/Pot.kt
@@ -56,8 +56,8 @@ internal object Pot : StatusEffectBasedBuff("Pot") {
                 // Check if there is any entity that we care about that can benefit from the potion
                 // This means we will only care about entities that are our enemies and are close enough to us
                 // That means we will still throw the potion if there is a friendly friend or team member nearby
-                val benefits = world.entitiesForRendering().filterIsInstance<LivingEntity>().any {
-                    it.shouldBeAttacked() && hasBenefit(it)
+                val benefits = world.entitiesForRendering().any {
+                    it is LivingEntity && it.shouldBeAttacked() && hasBenefit(it)
                 }
 
                 if (benefits) {
@@ -159,8 +159,9 @@ internal object Pot : StatusEffectBasedBuff("Pot") {
      * Check if the player is standing inside a lingering potion cloud
      */
     private fun isStandingInsideLingering() =
-        world.entitiesForRendering().filterIsInstance<AreaEffectCloud>().any {
-            it.distanceToSqr(player) <= BENEFICIAL_SQUARE_RANGE &&
+        world.entitiesForRendering().any {
+            it is AreaEffectCloud &&
+                it.distanceToSqr(player) <= BENEFICIAL_SQUARE_RANGE &&
                 it.potionContents.allEffects.any { effect ->
                 effect.effect == MobEffects.REGENERATION || effect.effect == MobEffects.INSTANT_HEALTH
                     || effect.effect == MobEffects.STRENGTH
@@ -171,8 +172,9 @@ internal object Pot : StatusEffectBasedBuff("Pot") {
      * Check if splash potion is nearby to prevent throwing a potion that is not needed
      */
     private fun isSplashNearby() =
-        world.entitiesForRendering().filterIsInstance<AbstractThrownPotion>().any {
-            it.distanceToSqr(player) <= BENEFICIAL_SQUARE_RANGE
+        world.entitiesForRendering().any {
+            it is AbstractThrownPotion &&
+                it.distanceToSqr(player) <= BENEFICIAL_SQUARE_RANGE
         }
 
 }

commit 29b91ef2ba0ff2046f4db91d8eebb6bbd571a116

fix(Theme): loading theme with command crashes (#7803)
caused by https://github.com/CCBlueX/LiquidBounce/commit/081c3078d2c40b513ebe824c24d55ee711c08b4e

src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleHud.kt
@@ -18,7 +18,6 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
-import net.ccbluex.fastutil.forEachIsInstance
 import net.ccbluex.liquidbounce.config.ConfigSystem
 import net.ccbluex.liquidbounce.config.types.group.ToggleableValueGroup
 import net.ccbluex.liquidbounce.config.types.group.ValueGroup
@@ -95,7 +94,8 @@ object ModuleHud : ClientModule("HUD", ModuleCategories.RENDER, state = true, hi
      * Updates [themes] content
      */
     fun updateThemes() {
-        themes.inner.forEachIsInstance<ValueGroup> {
+        // filterIsInstance then forEach to prevent ConcurrentModificationException
+        themes.inner.filterIsInstance<ValueGroup>().forEach {
             themes.drop(it)
         }
         for (theme in ThemeManager.themes) {